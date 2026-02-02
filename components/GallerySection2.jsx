822908dc3c11ca43dd1dc475f069c432a29da7ac
Sana-Backer <sanabacker08@gmail.com>
2026-02-02 15:13:18 +0530

    Added gallery videos and product images

--- components/GallerySection2.jsx
+++ components/GallerySection2.jsx
@@ -4,23 +4,92 @@ import { motion } from "framer-motion";
 
 const media = [
   {
+    type: "image",
+    src: "/products/coconutOil3.png",
+    className: "md:col-span-1 md:row-span-1",
+  },
+  {
     type: "video",
-    src: "/Dhanthapala-1.mp4",
+    src: "/gallery/g-video1.mp4",
     className: "md:col-span-2 md:row-span-2",
   },
   {
     type: "image",
-    src: "/gallery2.png",
+    src: "/products/chia-seed4.png",
     className: "md:col-span-1 md:row-span-1",
   },
   {
     type: "image",
-    src: "/aboutus-bg.jpg",
+    src: "/products/kesakala4.png",
     className: "md:col-span-1 md:row-span-1",
   },
   {
     type: "image",
-    src: "/about-2.jpg",
+    src: "/products/badaam4 (1).webp",
+    className: "md:col-span-1 md:row-span-1",
+  },
+
+
+  {
+    type: "video",
+    src: "/gallery/g-video3.mp4",
+    className: "md:col-span-2 md:row-span-2",
+  },
+  {
+    type: "image",
+    src: "/products/badhaam2.png",
+    className: "md:col-span-1 md:row-span-1",
+  },
+  {
+    type: "image",
+    src: "/products/dhandhapala2.jpeg",
+    className: "md:col-span-1 md:row-span-1",
+  },
+  {
+    type: "image",
+    src: "/products/flax-seed4.png",
+    className: "md:col-span-1 md:row-span-1",
+  },
+
+  {
+    type: "image",
+    src: "/products/rosemary-o.webp",
+    className: "md:col-span-1 md:row-span-1",
+  },
+
+
+
+  {
+    type: "image",
+    src: "/products/flax-seed3.png",
+    className: "md:col-span-1 md:row-span-1",
+  },
+  {
+    type: "image",
+    src: "/products/eucalyptus3.webp",
+    className: "md:col-span-1 md:row-span-1",
+  },
+  {
+    type: "video",
+    src: "/video2.mp4",
+    className: "md:col-span-2 md:row-span-1",
+  },
+
+
+
+  {
+    type: "image",
+    src: "/gallery/5.png",
+    className: "md:col-span-1 md:row-span-1",
+  },
+  {
+    type: "image",
+    src: "/gallery/image.jpeg",
+    className: "md:col-span-1 md:row-span-1",
+  },
+  {
+    type: "image",
+    src: "/gallery/8.png",
     className: "md:col-span-1 md:row-span-1",
   },
   {
@@ -29,24 +98,18 @@ const media = [
     className: "md:col-span-1 md:row-span-1",
   },
   {
-    type: "video",
-    src: "/video2.mp4",
-
-    className: "md:col-span-2 md:row-span-1",
-  },
-  {
     type: "image",
-    src: "/oil-bottless.png",
-    className: "md:col-span-1 md:row-span-2",
-  },
-  {
-    type: "image",
-    src: "/gallery/5.png",
+    src: "/black-seed.jpg",
     className: "md:col-span-1 md:row-span-1",
   },
   {
+    type: "video",
+    src: "/gallery/g-video4.mp4",
+    className: "md:col-span-2 md:row-span-2",
+  },
+  {
     type: "image",
-    src: "/gallery/4.png",
+    src: "/gallery/6.png",
     className: "md:col-span-1 md:row-span-1",
   },
   {
@@ -54,11 +117,24 @@ const media = [
     src: "/black-seed.jpg",
     className: "md:col-span-1 md:row-span-1",
   },
+
   {
-    type: "image",
-    src: "/gallery/1.png",
+    type: "video",
+    src: "/gallery/mill.mp4",
     className: "md:col-span-1 md:row-span-1",
   },
+
+  {
+    type: "video",
+    src: "/gallery/g-video2.mp4",
+    className: "md:col-span-2 md:row-span-2",
+  },
+  {
+    type: "image",
+    src: "/gallery/6.png",
+    className: "md:col-span-2 md:row-span-2",
+  },
+
 ];
 
 export default function GallerySection2() {
@@ -85,20 +161,20 @@ export default function GallerySection2() {
   };
 
   return (
-    <section id="gallery" className="bg-white px-4 md:px-10">
-      <div className=" mx-auto mb-5 flex flex-col md:flex-row md:items-end justify-between gap-6">
+    <section id="gallery" className="bg-white px-4 md:px-10 py-16">
+      <div className=" mx-auto mb-10 flex flex-col md:flex-row md:items-end justify-between gap-6">
         <motion.div
           initial={{ opacity: 0, x: -20 }}
           whileInView={{ opacity: 1, x: 0 }}
           viewport={{ once: true }}
           transition={{ duration: 0.8 }}
         >
-           <p className="text-sm tracking-widest text-gray-500 uppercase">
+          <p className="text-sm tracking-widest text-gray-500 uppercase">
             Gallery
           </p>
-          <h2 className="font-playfair text-[2rem] font-semibold md:text-[2rem] lg:[2.6rem] text-[#333333] md:whitespace-nowrap">
-            Our Signature Oils, 
-            <br/>
+          <h2 className="font-playfair text-[2rem] font-semibold md:text-[2rem] lg:text-[2.6rem] text-[#333333] md:whitespace-nowrap">
+            Our Signature Oils,
+            <br />
             Crafted with Tradition
           </h2>
         </motion.div>
@@ -111,18 +187,20 @@ export default function GallerySection2() {
           className="max-w-md md:text-right"
         >
           <p className="text-gray-500 text-lg font-light leading-relaxed">
-           Bring purity and tradition into <br/>
-your daily rituals with our finest oils.
+            Bring purity and tradition into <br />
+            your daily rituals with our finest oils.
           </p>
         </motion.div>
       </div>
+
+      {/* Hero Video Section */}
 
       <motion.div
         variants={containerVariants}
         initial="hidden"
         whileInView="visible"
         viewport={{ once: true, margin: "-50px" }}
-        className=" mx-auto grid grid-cols-2 md:grid-cols-4 gap-3 md:gap-2 auto-rows-[150px] md:auto-rows-[220px]"
+        className=" mx-auto grid grid-cols-2 md:grid-cols-4 gap-4 md:gap-6 auto-rows-[150px] md:auto-rows-[250px] grid-flow-dense"
       >
         {media.map((item, i) => (
 
